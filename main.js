8030fc9140d0e7a26a665d8d93165501e264ab29
Dalma Cersosimo <cersosimodalma@gmail.com>
2023-07-23 22:01:51 -0300

    commit del 23/7

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Proyecto llamado Farmaeducativa.
+
 //1) Página para registrarse
 
 let nombre = prompt("Ingrese su nombre");
@@ -26,66 +27,93 @@ console.log(contraseña);
 let consulta = prompt("Deja su consulta");
 console.log(consulta);
 
-//2) Cálculos de dosis
+//2) Cálculos de dosis- Esto va a la página "Arma tu pastillero"
 
-let pregunta = prompt("¿Tu medicación es en comprimidos?").toLowerCase();
+let foto = prompt("Tomar foto");
+console.log(foto);
+alert(foto);
 
-switch (pregunta){
- case"si":
-  alert("Continua con los cálculos");
-  let medicamento = prompt("¿Qué fármaco te recetó el médico");
-  console.log(medicamento);
-  alert(medicamento);
-  let miligramos = parseFloat(prompt("¿Cuántos miligramos tiene un comprimido"));
-  console.log(miligramos);
-  alert("Cada comprimido tiene" + " " + miligramos + " " + "mg");
-  
-  let comprimidos = parseFloat(
-    prompt("Ingrese cantidad de comprimidos en una caja")
-  );
-  console.log(comprimidos);
-  alert("Cada caja tiene" + " " + comprimidos + "comprimidos");
-  
-  alert(
-    "En una caja tienes" +
-      " " +
-      Number(miligramos) * Number(comprimidos) +
-      " " +
-      "mg"
-  );
-}
-  switch (pregunta){
-   case ("no"):
-   let medicamento = prompt("¿Qué fármaco te recetó el médico?");
-   console.log(medicamento);
-   alert(medicamento);
-   let mililitros = parseFloat(prompt("¿Cuántos mililitros tiene el frasco?"));
-   console.log(mililitros);
-   alert(" Cada frasco tiene" + " " + mililitros + " " + "ml"); 
-   let miligramos = parseFloat(prompt("¿Cuántos miligramos de fármaco tiene el frasco"));
-   console.log(miligramos);
-   alert("Tiene"+" " + miligramos +" "+ "mg" + " " + "en el frasco"); 
-   break;
-}
+let pregunta = prompt("¿Tu medicación es en comprimidos? Responder SI O NO").toLowerCase();
 
- 
-//4) Turno en la farmacia
-let turno = prompt("¿Quiere sacar un turno para una consulta?");
-if (turno ==="si") {
-  console.log(turno);
-alert("complete sus datos");  
-for (let turno = 1; turno <= 3; turno++) {
-    let apellido = prompt("Ingrese su Apellido y Nombre");
-    alert(`turno ${turno} : ${apellido}`); 
+switch (pregunta) {
+  case "si":
+    alert("Continua con los cálculos");
+    let medicamento = prompt("¿Qué fármaco te recetó el médico");
+    console.log(medicamento);
+    alert(medicamento);
+
+
+
+    let miligramos = parseFloat(
+      prompt("¿Cuántos miligramos tiene un comprimido")
+    );
+    console.log(miligramos);
+    alert("Cada comprimido tiene" + " " + miligramos + " " + "mg");
+
+    let comprimidos = parseFloat(
+      prompt("Ingrese cantidad de comprimidos en una caja")
+    );
+    console.log(comprimidos);
+    alert("Cada caja tiene" + " " + comprimidos + "comprimidos");
+
+    alert(
+      "En una caja tienes" +
+        " " +
+        Number(miligramos) * Number(comprimidos) +
+        " " +
+        "mg"
+    );
+   /*let entrada=prompt("¿cuántas veces al día su médico le indicó tomar la medicación");
+     while (entrada !=ESC);{
+      alert(`Se ingresó ${entrada}`);
+      entrada=prompt("Ingrese un dato");
+     }*/
     
-  }
-alert(" Se acabaron los turnos");
 }
   
-
-
-
-//5)
+  switch (pregunta) { 
+  case "no":
+    let medicamento = prompt("¿Qué fármaco te recetó el médico?");
+    console.log(medicamento);
+    alert(medicamento);
+    let mililitros = parseFloat(prompt("¿Cuántos mililitros tiene el frasco?"));
+    console.log(mililitros);
+    alert(" Cada frasco tiene" + " " + mililitros + " " + "ml");
   
+    let dosis = parseFloat(prompt(
+      "¿Cuál es la dosis por kilogramo de peso de la persona y por día?"
+    ))
+    console.log(dosis);
+    alert("La dosis por kilogramo de peso es" + " " + dosis + " " + "mg");
+
+    let pesoPersona = parseFloat(prompt("¿Cuál es el peso de la persona?"));
+    console.log(pesoPersona);
+
+    alert("La persona" + " " + "pesa" + " " + pesoPersona + " " + "kg");
+
+
+    let vasitoMg = parseFloat(prompt("cantidad de mg que hay en 5 ml, ( mirar en la caja del jarabe)"));
+    console.log(vasitoMg);
+    alert("En 5 ml hay"+" "+ vasitoMg + " " + "mg");
+    
+   
+    const calculo = (pesoPersona, dosis, vasitoMg) => ((pesoPersona*dosis)/3 * 5) /vasitoMg;
+     
+    alert("La persona tomará" +" "+ calculo +" "+  "ml");
+  }
+
+//4 ) Turno en la farmacia
+let turno = prompt("¿Quiere sacar un turno para una consulta?");
+if (turno === "si") {
+  console.log(turno);
+  alert("complete sus datos");
+  for (let turno = 1; turno <= 3; turno++) {
+    let apellido = prompt("Ingrese su Apellido y Nombre");
+    alert(`turno ${turno} : ${apellido}`);
+
+} 
+  alert("Se acabaron los turnos")
+}
+
 
 
